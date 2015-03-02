6791ad7299d139eac42d26dda345698c8385ae65
ematevosyan <ematevosyan@webbfontaine.am>
2015-03-03 01:03:43 +0400

    Added new mail configs.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -146,10 +146,14 @@ grails.mail.default.bcc = 'emails@scholarmap.com'
 
 grails {
     mail {
-        host = "smtp.host"
-        port = 25
-        username = "username"
-        password = "password"
+        host = "smtp.gmail.com"
+        port = 465
+        username = "scholarmap@gmail.com"
+        password = "scholarmap123456"
+        props = ["mail.smtp.auth"                  : "true",
+                 "mail.smtp.socketFactory.port"    : "465",
+                 "mail.smtp.socketFactory.class"   : "javax.net.ssl.SSLSocketFactory",
+                 "mail.smtp.socketFactory.fallback": "false"]
     }
 }
 
