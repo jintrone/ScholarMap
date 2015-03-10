1e7441807a8b533e2bdbc22dd3aed39d842f3340
Emil Matevosyan <emil.matevosyan@gmail.com>
2015-03-11 03:17:44 +0400

    Added test for ReferenceService.

---
+++ test/unit/csst15/ReferenceServiceSpec.groovy
@@ -0,0 +1,24 @@
+package csst15
+
+import csst15.command.ReferenceCommand
+import csst15.security.User
+import grails.buildtestdata.mixin.Build
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(ReferenceService)
+@Build([User, Reference])
+class ReferenceServiceSpec extends Specification {
+
+    void "test create reference method"() {
+        given:
+        def user = User.build()
+        def citation = 'citation'
+        def command = new ReferenceCommand(year: '2014', content: 'content', citation: citation)
+        service.springSecurityService = [currentUser: user]
+
+        expect:
+        service.createReference(command).creator == user
+        service.createReference(command).citation == citation
+    }
+}
